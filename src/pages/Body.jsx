72554bbbc38b5b0b8cd6eb071a3fec0f7f51fa47
DitGrau <juditgraup@gmail.com>
2023-09-17 11:31:57 +0200

    "feat: body router"

---
+++ src/pages/Body.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./Home"
+
+export function Body() {
+
+
+    return (
+        <Routes>
+            <Route path='/' element={<Home />} />
+        </Routes>
+    )
+}
