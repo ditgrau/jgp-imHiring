c6e9d2427b383819bd23163aebe1b8f04dcf35e3
DitGrau <juditgraup@gmail.com>
2023-09-17 11:30:38 +0200

    "feat: firsts steps, main structure"

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import { Body } from './pages/Body'
+import { Navbar } from './components/Navbar/Navbar'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Navbar />
+      <Body />
     </>
   )
 }
